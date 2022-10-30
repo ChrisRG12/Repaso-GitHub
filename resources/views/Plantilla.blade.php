0ac02bef1c3722b6c6ccdd9a1f85c947e8b3e046
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-29 23:24:40 -0500

    Principal
    Agregaremos una tarjeta con un header, un body y un footer para la noticia literaria

--- resources/views/Plantilla.blade.php
+++ resources/views/Plantilla.blade.php
@@ -32,7 +32,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Principal</a>
+                <a class="nav-link active" aria-current="page" href="Principal">Principal</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Registro</a>
