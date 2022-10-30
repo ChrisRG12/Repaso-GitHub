43243edfefa9142a60cd6798f2b67c7c6bf83794
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-30 17:54:15 -0600

    Formulario
    Agregamos una tarjeta en la vista del registro con un form, para que podamos registrar el libro y tengamos una  mejor vista y sea mas entendible.

--- resources/views/Registro.blade.php
+++ resources/views/Registro.blade.php
@@ -3,13 +3,32 @@
 
 @section('contenido')
 
+<div class="container mt-5  cold-md-8">
+
+    <div class="card text-center mb-5">
+        <form action="#">
+
+
+        <div class="card-header fw-bolder">
+           Registro  de Libros 
+        </div>
+
+        <div class="card-body ">
+ 
+        </div>
+
+        <div class="card-footer text-center">
 
 
 
+        </form>
+        
+        </div>
+
+    </div>
 
 
-
-
+</div>
 
 
 
