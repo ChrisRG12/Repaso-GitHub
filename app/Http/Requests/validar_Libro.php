ee510c92f7208702f9d947c68b8e2c5ba75a092c
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-30 18:46:58 -0600

    Espeficiaciones
    En el archivo de validadores, especificamos los campos necesarios y los mínimos o máximos de cada input.,

---
+++ app/Http/Requests/validar_Libro.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class validar_Libro extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'NumISNB' => 'required|min:13',
+            'txtTitulo' => 'required',
+            'txtAutor' => 'required',
+            'NumPaginas' => 'required',
+            'txtEditorial' => 'required',
+            'txtEmail' => 'required'
+        ];
+    }
+}
