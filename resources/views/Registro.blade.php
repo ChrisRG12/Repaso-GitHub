342c6bda29d90f19700fe531ab854586f5ad1871
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-30 18:06:08 -0600

    Inputs
    Agregamos los inputs para el registro de datos y del libro

--- resources/views/Registro.blade.php
+++ resources/views/Registro.blade.php
@@ -6,6 +6,7 @@
 <div class="container mt-5  cold-md-8">
 
     <div class="card text-center mb-5">
+
         <form action="#">
 
 
@@ -14,7 +15,41 @@
         </div>
 
         <div class="card-body ">
- 
+
+            <div class="mb-3">
+            <label class="form-label"> ISNB</label>
+            <input type="Number" class="form-control" name="NumISNB" >
+            </div>
+
+
+            <div class="mb-3">
+             <label class="form-label"> Titulo</label>
+             <input type="text" class="form-control" name="txtTitulo">
+            </div>
+
+
+            <div class="mb-3">
+              <label class="form-label"> Autor</label>
+             <input type="text" class="form-control" name="txtAutor">
+            </div>
+
+
+            <div class="mb-3">
+             <label class="form-label"> Paginas</label>
+             <input type="Number" class="form-control" name="NumPaginas">
+            </div>
+
+            <div class="mb-3">
+             <label class="form-label"> Editorial</label>
+             <input type="text" class="form-control" name="txtEditorial">
+            </div>
+
+            <div class="mb-3">
+             <label class="form-label"> Email Editorial </label>
+             <input type="Email" class="form-control" name="txtEmail">
+            </div>
+
+
         </div>
 
         <div class="card-footer text-center">
@@ -22,7 +57,7 @@
 
 
         </form>
-        
+
         </div>
 
     </div>
