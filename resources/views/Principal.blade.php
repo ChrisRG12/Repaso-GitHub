912a60e1436554121366c9aabd71959fa4df2aa0
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-31 15:27:31 -0600

    Imagen Funcional
    Hacemos funcional la imagen de la noticia cada que la aprieten la redireccionara a  la noticia.

--- resources/views/Principal.blade.php
+++ resources/views/Principal.blade.php
@@ -12,7 +12,8 @@
             Noticia
         </div>
 
-        <img src="\Imagenes\dia.jpg" class="card-img-top" alt="...">
+         <a href="https://leseg.unav.edu/2022/10/24/24-de-octubre-dia-de-las-bibliotecas/">
+        <img src="\Imagenes\dia.jpg" class="card-img-top" alt="..."></a>
 
         <div class="card-body">
             <p class="card-text ">El Día de las Bibliotecas se celebra cada 24 de octubre desde el año 1997. Este día nació como iniciativa de la Asociación Española de Amigos del Libro Infantil y Juvenil con el objetivo de concienciar a la sociedad de la importancia de la lectura y como homenaje y reconocimiento a la labor de los bibliotecarios/as.</p>
