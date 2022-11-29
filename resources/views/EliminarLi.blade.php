c900771175d6ca2fa7fb54b94272d2769ad2b20e
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-11-28 23:11:19 -0600

    A la vista creada para eliminar le pasamos los datos de la tabla NO TODOS solo,le pase el nombre y lo eso para poder eliminar

--- resources/views/EliminarLi.blade.php
+++ resources/views/EliminarLi.blade.php
@@ -14,28 +14,29 @@
          
       <div class="card text-center">
   
-        <form method="POST" action="{{route('Autor.destroy', $consultaId->idAutor)}}">
+        <form method="POST" action="{{route('Libro.destroy', $consultaId->idLibro)}}">
           @csrf
   
           {!!method_field('DELETE')!!}
   
           <div class="card-header">
-            <h5 class="text-primary text-center"> Autor </h5>
+            <h5 class="text-primary text-center"> Libro </h5>
           </div>
   
           <div class="card-body">
   
-            <h5 class="card-title">  {{ $consultaId->Nombre  }}</h5>
-            <p class="card-text">  {{ $consultaId->Fechanacimiento  }} </p>
-            <p class="card-text">  {{ $consultaId->Librospublicados  }} </p>
+            <h5 class="card-title">  {{ $consultaId->Titulo  }}</h5>
+            <p class="card-text">  {{ $consultaId->ISBN  }} </p>
+            <p class="card-text">  {{ $consultaId->paginas  }} </p>
+            <p class="card-text">  {{ $consultaId->editorial  }} </p>
 
            
           </div>
   
           <div class="card-footer text-muted">
-            <button type="submit" class="btn btn-danger">Eliminar Autor</button>
+            <button type="submit" class="btn btn-danger">Eliminar Libro</button>
              
-          <a href="{{route('Autor.index')}}" class="btn btn-secondary"> No, Me arrepenti</a>
+          <a href="#" class="btn btn-secondary"> No, Me arrepenti</a>
           </div>
         </form>
   
